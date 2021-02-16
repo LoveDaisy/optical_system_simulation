0983372b5f5113b606f829709870e1a0868fb6b3
zhangjiajie <zhangjiajie@kuaishou.com>
2021-02-16 17:32:11 +0800

    refactor getGaussianBackWorkingLength

--- @OpticalSystem/getGaussianBackWorkingLength.m
+++ @OpticalSystem/getGaussianBackWorkingLength.m
@@ -1,16 +1,15 @@
 function l = getGaussianBackWorkingLength(obj, lambda)
-% INPUT
-%   obj:        OpticalSystem object
-%   lambda:     n-length vector
-% OUTPUT
-%   l:          n-length vector, the gaussian rear working length
+% SYNTAX
+%   l = sys.getGaussianBackWorkingLength(lambda)
+% where
+%   lambda:     n-length vector, wavelength
+%   l:          n-length vector, the gaussian back working length
 
-OpticalSystem.check1D(lambda);
+parser = inputParser;
+parser.FunctionName = 'getGaussianBackWorkingLength';
+parser.addRequired('lambda', @(x) isnumeric(x) && isreal(x) && isvector(x));
+parser.parse(lambda);
 
 sys_mat = obj.makeGaussianSystemMatrix(lambda);
-wl_num = size(sys_mat, 3);
-l = zeros(size(lambda));
-for i = 1:wl_num
-    l(i) = -sys_mat(2, 2, i) / sys_mat(1, 2, i);
-end
+l = squeeze(-sys_mat(2, 2, :) ./ sys_mat(1, 2, :));
 end
